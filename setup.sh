8920e4bbc1a3c82561cfb78248adf5d55b4a9279
Sergei Petrunia <psergey@askmonty.org>
2019-03-07 21:11:01 +0000

    Stash away the original, empty datadirs

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 
 ./setup-server/setup-fbmysql-clone.sh range-locking range-locking-fb-mysql-5.6.35
-./setup-server/setup-fbmysql-clone.sh orig fb-mysql-5.6.35-rebase1-source
+./setup-server/setup-fbmysql-clone.sh orig          range-locking-fb-mysql-5.6.35-orig
 
+cp -r data-fbmysql-orig data-fbmysql-orig.clean
+cp -r data-fbmysql-range-locking data-fbmysql-range-locking.clean
 
