8b6c09b70f8aeda0a48e0dd78a477742f63a65ea
Sergei Petrunia <psergey@askmonty.org>
2021-05-08 18:18:40 +0300

    set-fbmysql8.sh : use post-rebase branch

--- setup-fbmysql8.sh
+++ setup-fbmysql8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./setup-server/setup-fbmysql8-clone.sh range-locking fb-mysql-8.0.17-range-locking-jan21
-./setup-server/setup-fbmysql8-clone.sh orig          fb-mysql-8.0.17-range-locking-jan21-base
+./setup-server/setup-fbmysql8-clone.sh range-locking fb-mysql-8.0.20-range-locking-mar
+./setup-server/setup-fbmysql8-clone.sh orig          fb-mysql-8.0.20-range-locking-mar-base
 
 echo "rocksdb_use_range_locking=1" >>  my-fbmysql-range-locking.cnf
 
