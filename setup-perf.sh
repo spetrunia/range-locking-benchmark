0ec988f6eef5814e5f89d8d15de5e959f0b9268e
Sergei Petrunia <psergey@askmonty.org>
2021-05-10 13:04:51 +0300

    Proper steps to setup perf

--- setup-perf.sh
+++ setup-perf.sh
@@ -1,2 +1,8 @@
-sudo apt install linux-tools-common          # version 5.4.0-66.74
-sudo apt -y install linux-tools-aws  linux-cloud-tools-aws
+#!/bin/bash
+
+#sudo apt install linux-tools-common          # version 5.4.0-66.74
+#sudo apt -y install linux-tools-aws  linux-cloud-tools-aws
+
+sudo apt -y install linux-tools-common linux-tools-aws
+sudo apt -y install linux-tools-5.4.0-1045-aws
+
